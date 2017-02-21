d5370667bcf411cf507eaade6625537d2f0a4d60
Arjun  Dass <ad.sam007@gmail.com>
2017-02-21 16:43:14 -0500

    Added five test cases to user story 7
    For user story ageLessThan150

--- unitTest.py
+++ unitTest.py
@@ -22,3 +22,20 @@ class TestMarriageBeforeDeath_US05(TestCase):
 
     def test_marriageBeforeDeath_US05_ReturnsNone(self):
         self.assertIsNone(parser.marriageBeforeDeath_US05(fam, ind), "ERROR: Family: US05: Returns None Function can't return None.")
+
+#implemnted test cases for user story 07
+class TestAgeLessThan150_US7(TestCase):
+    def test_ageLessThan150_US7_ageIsNone(self):
+        self.assertIsNotNone(ind[0]['age'], "ERROR: Individual: US07: Age " + str(ind[0]['age']) + " Age cannot be None.")
+
+    def test_ageLessThan150_US7_isInteger(self):
+        self.assertEqual(type(ind[0]['age']),int, "ERROR: Individual: US07: Age " + str(ind[0]['age']) + "Age is not an Integer.")
+
+    def test_ageLessThan150_US7_ageIsNegative(self):
+        self.assertGreater(ind[0]['age'], 0, "ERROR: Individual: US07: Age " + str(ind[0]['age']) + " Age can't be negative.")
+
+    def test_ageLessThan150_US7_ageIsFloat(self):
+        self.assertNotEquals(type(ind[0]['age']), float, "ERROR: Individual: US07: Age " + str(ind[0]['age']) + " Age can't be float.")
+
+    def test_ageLessThan150_US7_ReturnsFalse(self):
+        self.assertFalse(parser.ageLessThan150_US7(ind), "ERROR: Individual: US07: Returns False, Function can't return False.")
